49ee4621f1c7886d4d1377a36eb7c1bea82ab812
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2024-01-12 11:56:08 -0800

    [update] saving progress

---
+++ scripts/build-prefix-trie.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const { createWriteStream } = require("fs")
+
+const MOVIES_DATA_PATH = path.resolve(process.cwd(), "./data/movies.json")
+const PREFIX_TRIE_DATA_PATH = path.resolve(process.cwd(), "./data/prefix-trie.json")
+
+const EOF = "EOF"
+
+const movies = require(MOVIES_DATA_PATH)
+
+const createPrefixTrie = (movies, trie = {}) => {
+  // const trie = {}
+
+  movies.forEach((movie) => {
+    const prefixes = []
+
+    for (let i = 0; i < movie.title.length; i++) {
+      prefixes.push(movie.title.slice(i, movie.title.length).toLowerCase())
+    }
+
+    // console.log("prefixes", prefixes)
+
+    prefixes.forEach((prefix) => {
+      let currentNode = trie
+      prefix.split("").forEach((char) => {
+        const lowerChar = char.toLowerCase()
+
+        if (!currentNode[lowerChar]) {
+          currentNode[lowerChar] = {}
+        }
+
+        currentNode = currentNode[lowerChar]
+
+        if (!currentNode[EOF]) {
+          currentNode[EOF] = false
+        }
+      })
+      currentNode[EOF] = true
+    })
+
+    trie[EOF] = true
+  })
+
+  return trie
+}
+
+const prefixTrie = createPrefixTrie(movies, {})
+const prefixTrieStream = createWriteStream(PREFIX_TRIE_DATA_PATH, { flags: "w" })
+prefixTrieStream.write(JSON.stringify(prefixTrie, null, 2))
+prefixTrieStream.end()
+
+// console.log("prefixTrie", JSON.stringify(prefixTrie, null, 2))
