7a8c470bf09dbf5b997344c49106e028c95473cc
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2024-01-13 21:53:25 -0800

    [update] saving progress

--- app/components/Autocomplete/index.tsx
+++ app/components/Autocomplete/index.tsx
@@ -1,5 +1,26 @@
+import { useQuery } from "@tanstack/react-query"
 import Autocomplete from "@/app/components/Autocomplete/Autocomplete"
+import type { Movie } from "@/types"
+import { baseUrl } from "@/app/constants"
+import { stringify } from "qs"
+
+const fetchMovies = async (page: number = 1): Promise<Movie[]> => {
+  const params = {
+    page,
+    query: "",
+  }
+  const response = await fetch(
+    `${baseUrl}/api/v1/search${stringify(params, { addQueryPrefix: true })}`
+  )
+  const data = (await response.json()) as { results: Movie[] }
+  return data.results
+}
 
 export default function AutoCompleteContainer() {
+  const { data: movies } = useQuery({
+    queryKey: ["movies", { page: 1 }],
+    queryFn: ({ queryKey }) => fetchMovies((queryKey[1] as { page: number }).page),
+  })
+  console.log(movies)
   return <Autocomplete />
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 "use client"
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { NextUIProvider } from "@nextui-org/react"
 import AutoComplete from "@/app/components/Autocomplete"
 
+const queryClient = new QueryClient()
+
 export default function Home() {
   return (
     <NextUIProvider>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1>Aloha!</h1>
-        <div className="mt-5">
-          <AutoComplete />
-        </div>
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+          <h1>Aloha!</h1>
+          <div className="mt-5">
+            <AutoComplete />
+          </div>
+        </main>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </NextUIProvider>
   )
 }
